60844848a8b00f96d3d519944eb12cf568276ff2
Samuel Lee <slee0258@gmail.com>
2022-12-16 03:42:45 +0000

    Only completed the first two practice questions.

---
+++ javascript-loops-practice/get-indexes.js
@@ -0,0 +1,11 @@
+/* exported getIndexes */
+/*
+  - create function that holds array as the parameter
+  - within the function code block make a variable and
+  assign it to an empty array
+  - write a for loop to loop through each index in the array for the
+  argument in the called function getIndexes
+  - take out the each individual index from the loop and push into the
+  new array
+  -return the new array with the indexes out of the function
+*/
