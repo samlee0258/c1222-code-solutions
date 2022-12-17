7849211d76e3159986e7a27de736e482e86688f0
Samuel Lee <slee0258@gmail.com>
2022-12-17 03:14:16 +0000

    In progress

---
+++ string-manipulation-1/src/capitalize.js
@@ -0,0 +1,13 @@
+/* exported capitalize */
+/*
+  - define a function named capitalize with one parameter "word"
+  - get the first character of the string and uppercase it by using the charAt()
+  method to get the first character and the toUpperCase() method to uppercase the
+  string value
+  - return the output
+*/
+
+function capitalize(word) {
+  word.toLowerCase(word);
+  return word[0].toUpperCase();
+}
