f519d6fc35ac005f0b6c313ff75e809403ee30c3
Samuel Lee <slee0258@gmail.com>
2022-12-19 19:07:13 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,20 @@
+var $heading = document.querySelector('h1');
+
+console.log('$heading', $heading);
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+
+console.log('$explanation', $explanation);
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+
+console.log('$hint', $hint);
+console.dir($hint);
+
+var $paragraph = document.querySelectorAll('p');
+console.log('$paragraph', $paragraph);
+
+var $example = document.querySelectorAll('.example-link');
+console.log('$example', $example);
