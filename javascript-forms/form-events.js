e2074ee59cd815ef78c55b18e79a245434a7c5fc
Samuel Lee <slee0258@gmail.com>
2022-12-20 05:18:49 +0000

    In progress.

---
+++ javascript-forms/form-events.js
@@ -0,0 +1,14 @@
+// function handleFocus(event) {
+//   console.log('focus');
+//   console.log(event.target.name);
+// }
+
+// function handleBlur(event) {
+//   console.log('blur');
+//   console.log(event.target.name);
+// }
+
+// function handleInput(event) {
+//   console.log(event.target.name);
+//   console.log(event.target.value);
+// }
