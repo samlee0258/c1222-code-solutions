659fcb445db8feed2f745a5868640d5dbbf53005
Samuel Lee <slee0258@gmail.com>
2022-12-24 07:51:33 +0000

    In progress.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -2,9 +2,18 @@
   -
 */
 
-// var $button = document.querySelector('button');
-// $button.addEventListener('click', toggle);
+var $openModal = document.querySelector('.open-modal');
+var $closeModal = document.querySelector('.close-modal');
+var $body = document.querySelector('body');
+$openModal.addEventListener('click', toggle);
+$closeModal.addEventListener('click', toggle);
 
-// function toggle(event) {
-//   console.log('click');
-// }
+function toggle(event) {
+  if ($openModal.className === 'open-modal') {
+    $body.className = 'none';
+    $closeModal.className = 'block';
+  } else {
+    $openModal.className = 'open-modal';
+    $closeModal.className = 'none';
+  }
+}
