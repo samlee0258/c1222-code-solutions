2dbe4131e5ee764051434e08a1caf99a4bda2888
Samuel Lee <slee0258@gmail.com>
2022-12-27 00:34:48 +0000

    Completed object-manipulation-1 practice.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -2,19 +2,17 @@
 /*
   - define a function named toObject with one parameter "keyValuePair"
   - create a variable assigned to an empty object
+  - create a variable 'key' assigned to the keyValuePair at index 0
+  - create a variable 'value' assigned to the keyValuePair at index 1
   - take the key and its value and store into output object
   - return output object
 */
 
 function toObject(keyValuePair) {
+  var object = {};
   var key = keyValuePair[0];
   var value = keyValuePair[1];
-  var object = {};
-  object.key = key;
-  object.value = value;
-  // for (var key in keyValuePair) {
-  //   keys.push(key);
-  //   values.push(keyValuePair[key]);
-  // }
+  object[key] = value;
+
   return object;
 }
