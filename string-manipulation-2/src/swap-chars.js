260bd5572525103b21c0af6832e660734322115c
Samuel Lee <slee0258@gmail.com>
2023-01-01 21:57:01 +0000

    Finished writing pseudo code for practice questions.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,4 +1,9 @@
 /* exported swapChars */
 /*
-
+  - create a function named swapChars with 3 parameters "firstIndex", "secondIndex", and "string"
+  - convert the string into an array
+  - assign the firstIndex to the secondIndex
+  - assign the secondIndex to the firstIndex
+  - convert the array to a string
+  - return string with swapped indexes.
 */
