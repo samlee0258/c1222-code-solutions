0bf33f71e9d5110c54761a9b1a0d03a2f358b0a4
Samuel Lee <slee0258@gmail.com>
2023-01-10 08:14:44 +0000

    Completed string-manipulation-2 practice problems.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,20 +1,28 @@
 /* exported capitalizeWord */
 /*
   - create a function named capitalizeWord with one parameter "word"
-  - first get the string and lowercase all the characters in the string
-  - then uppercase just the first character of the string
-  - if "JavaScript" only the "J" and "S" should be uppercased
-  - return the string with the right characters capitalized
+  - create a variable named outputArr and assign it to an empty array
+  - lowercase all the characters in the string
+  - if word strictly equals 'javascript'
+    - uppercase the character at the 0 index in the string and concatenate it with the lowercased characters
+    at index 1 through 4 using the charAt() method and toUpperCase() method to get "J" and slice() method
+    to get the string "ava"
+    - concatenate "Java" with the uppercase 4th index and concatenate result of that expression with the lowercased
+    characters at index 5 through 10 using the charAt() method and toUpperCase() method to get "S" and slice() method
+    - push the result of the expression into the output array
+    - return the string "JavaScript" using the join() method
+  - else get the uppercase character at the index of 0 and concatenate with the characters starting
+  at the 1 index
+  - return string
 */
 
 function capitalizeWord(word) {
-  var arr = [];
+  var outputArr = [];
   word = word.toLowerCase();
-  if (word === 'JavaScript') {
-    arr.push(word.charAt(0).toUpperCase() + word.slice(1, 3));
-    arr.push(word.charAt(4).toUpperCase() + word.slice(5, 9));
-    return arr;
+  if (word === 'javascript') {
+    outputArr.push(word.charAt(0).toUpperCase() + word.slice(1, 4) + word.charAt(4).toUpperCase() + word.slice(5, 10));
+    return outputArr.join('');
   } else {
-    return word.charAt(0).toUpperCase() + word.slice(1, word.length).toLowerCase();
+    return word.charAt(0).toUpperCase() + word.slice(1);
   }
 }
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -3,21 +3,22 @@
   - create a function named numVowels with one parameter named "string"
   - create a variable vowels assigned an array of vowels
   - create a variable vowelCount assigned the number 0
-  - create a for loop to loop through the string
-    - if the character at the string index is a vowel
-      - increase the vowelCount by 1
+  - create a variable assigned an array with each character of the string
+  - create a for loop to loop through the array with the broken up string
+    - create a for loop to loop through the vowels array
+      - if the character at the eachChar array strictly equals to the vowels index
+      - increase vowelCount
   - return vowelCount
 */
 
 function numVowels(string) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var vowels = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'];
   var vowelCount = 0;
-  var eachLetter = string.split('');
-  for (var i = 0; i < eachLetter.length; i++) {
-    for (var k = 0; k < vowels.length; k++) {
-      if (eachLetter[i] === vowels[k]) {
+  var eachChar = string.split('');
+  for (var char = 0; char < eachChar.length; char++) {
+    for (var index = 0; index < vowels.length; index++) {
+      if (eachChar[char] === vowels[index]) {
         vowelCount++;
-        break;
       }
     }
   }
